4ae4d608809c094b5756ca209f09551c38235102
drduck0 <anandsivanandam03@gmail.com>
2023-06-19 15:23:30 +0530

    Login page
    Signed-off-by: drduck0 <anandsivanandam03@gmail.com>

--- RV/decorator.py
+++ RV/decorator.py
@@ -2,9 +2,10 @@ from django.shortcuts import redirect
 
 
 def unauthenticated_user(view_func):
-    def wrapper(request, *arg, **kwargs):
-        if not request.user.is_authenticated:
+    def wrapped_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
             return redirect('home')
         else:
-            return view_func(request, *arg, **kwargs)
-    return wrapper
+            return view_func(request, *args, **kwargs)
+    return wrapped_func
+ 
--- RV/views.py
+++ RV/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from .decorator import unauthenticated_user
+
+
+def dashboard(request):
+    return render(request, 'RV/dashboard.html')
+
 
 @login_required(login_url='login')
 def home(request):
@@ -16,34 +22,33 @@ def login_page(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        user = authenticate(request, username = username, password = password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
-            messages.info(request, 'Username or Password is incorrect')
+            messages.info(request, 'username or password is incorrect')
 
-    return render(request, 'RV/login.html')
+    context = {}
+    return render(request, 'RV/login.html', context)
 
-
-@unauthenticated_user
-def register_page(request):
-    form = CreateUserForm()
-    context = {'form':form}
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user_name = form.cleaned_data.get('username')
-            messages.success(request, 'Profile Created for ' + user_name)
-            return redirect('login')
-        
-    return render(request, 'RV/register.html', context)
 
 def logout_user(request):
     logout(request)
     return redirect('login')
 
-def dashboard(request):
-    return render(request, 'RV/dashboard.html')
+
+@unauthenticated_user
+def register_page(request):
+    form = CreateUserForm()
+    context = {'form': form}
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Profile Created for ' + user)
+            return redirect('login')
+
+    return render(request, 'RV/register.html', context)
