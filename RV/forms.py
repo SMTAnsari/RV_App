c63b4dca1c6ed189b8285e382819e455ebd1edaf
S Mohamed Thamimul Ansari <mohamedthamimulansari07@gmail.com>
2023-06-20 15:15:44 +0530

    css work
    Signed-off-by: S Mohamed Thamimul Ansari <mohamedthamimulansari07@gmail.com>

--- RV/forms.py
+++ RV/forms.py
@@ -1,3 +1,6 @@
+ 
+
+
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -5,7 +8,7 @@ from django.forms import forms
 from django import forms
 
 class CreateUserForm(UserCreationForm):
-    phone_number = forms.CharField(label='Phone Number', max_length=10, widget=forms.TextInput(attrs={'type': 'tel', 'pattern': '[0-9]{10}', 'title': 'Please enter a 10-digit phone number','value': '+91'}))
+    phone_number = forms.CharField(label='Phone Number', max_length=10, widget=forms.TextInput(attrs={'type': 'tel', 'pattern': '[0-9]{10}', 'title': 'Please enter a 10-digit phone number'}))
     class Meta:
         model = User
         fields = ['username', 'email','phone_number', 'password1', 'password2']
